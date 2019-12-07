f46d89272747b7b94a5c7f8f95d7f280b482549d
yibinouyang <43450746+yibinouyang@users.noreply.github.com>
2019-12-07 16:38:36 -0500

    node.js
    node.js

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,20 @@
 var express = require('express');
 var router = express.Router();
-var path = require('path');
+// var path = require('path');
 
 const sql = require('../utils/sql');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log('at the main route');
-  let query = "SELECT ID,Founder, Representative, Peculiarity, Representative animal, alumni, image FROM tbl_poster";
+  let query = "SELECT ID,name, members, image FROM tbl_portfolio";
 
   sql.query(query, (err, result) => {
     if (err) { throw err; console.log(err); }
 
     console.log(result);
 
-    res.render('index',{ house:result });
+    res.render('index',{ works:result });
   })
   
 })
